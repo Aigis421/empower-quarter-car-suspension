e8e58d0389def374730fc39397104be6b320c7a6
Sal46c <salomeponce46@gmail.com>
2026-07-21 15:36:04 -0700

    Update Simscape_Matlab.m

--- Simscape_Matlab.m
+++ Simscape_Matlab.m
@@ -1,23 +1,38 @@
 clear;
 
-Ks = 30647.196473;
-c = 800;
-M = 108;
-m = 50;
-Kt = 114182.69772;
+Ks = 30647.196473; % Suspension spring constant
+c = 700;           % Suspension damping constant
+M = 108;           % Mass of Car
+m = 50;            % Mass of wheel
+Kt = 114182.69772; % Tire Spring Constant
 
 disp('1. Smooth speed bump')
 disp('2. Rough terrain')
 disp('3. Smooth median strip')
 disp('4. Rough terrain + Pothole')
 %select = input("Select Road Profile: ");
-select = 4;
+select = 1;
 
+% Simulate suspension system and opens simulink
 model = 'MassSpringDamper_simulink';
 open_system(model);
 Smodel = sim(model);
 
+% Extracts data from Simulink
 t = Smodel.tout;
 S_pos = Smodel.SprungMassPos.Data;
+US_pos = Smodel.UnSprungMassPos.Data;
+Zt = Smodel.RoadProfile.Data;
+
+S_vel = Smodel.SprungMassVel.Data;
+US_vel = Smodel.UnSprungMassVel.Data;
+
+S_Accel = Smodel.SprungMassAccel.Data;
+US_Accel = Smodel.UnSprungMassAccel.Data;
+
+% Ride Analysis
+Comfort_m = rms(S_Accel);
+max_travel = max(S_pos);
+deflection = US_pos - Zt;
 
 plot(t,S_pos)
