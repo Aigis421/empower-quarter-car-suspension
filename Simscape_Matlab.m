71dae619ec922eab9934013876b434bfadc8ab01
Sal46c <salomeponce46@gmail.com>
2026-07-22 14:41:59 -0700

    Update Simscape_Matlab.m

--- Simscape_Matlab.m
+++ Simscape_Matlab.m
@@ -6,6 +6,11 @@ M = 108;           % Mass of Car
 m = 50;            % Mass of wheel
 Kt = 114182.69772; % Tire Spring Constant
 
+names = {'Smooth Speed Bump','Rough Terrain','Washboards','Rough Terrain + Pothole'};
+% 1 = Smooth speed bump
+% 2 = Rough Terrain
+% 3 = Washboards
+% 4 = Rough Terrain + Pothole
 
 % Simulate suspension system and opens simulink
 for S = 1:4
@@ -15,7 +20,6 @@ open_system(model);
 Smodel = sim(model);
 
 % Assigns Data based on road profile simulation to "Data" struct 
-names = {'Smooth Speed Bump','Rough Terrain','Smooth Median Strip','Rough Terrain + Pothole'};
 Data(S).name   = names{S};
 Data(S).t      = Smodel.tout;
 Data(S).Zt     = Smodel.RoadProfile.Data;
@@ -31,16 +35,18 @@ Data(S).comfort    = rms(Data(S).S_Acc);
 Data(S).maxTravel  = max(Data(S).S_pos);
 Data(S).deflection = Data(S).US_pos - Data(S).Zt;
 
+% Plotting car position
 figure(1)
 set(gcf, 'Name', 'Car position')
 subplot(2,2,S)
 plot(Data(S).t, Data(S).S_pos)
 title(Data(S).name) 
 
+% Plotting Tire Position
 figure(2)
 set(gcf, 'Name', 'Tire position')
 subplot(2,2,S)
 plot(Data(S).t, Data(S).US_pos)
-title(Data(S).name, 'Tire position')
+title(Data(S).name)
 
 end
