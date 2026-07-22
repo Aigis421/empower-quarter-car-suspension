c317930c30440966219ba68145ab8b679e99daaa
Sal46c <salomeponce46@gmail.com>
2026-07-22 14:21:14 -0700

    Update Simscape_Matlab.m
    plots both sprung and unsprung masses as well as labels graphs accordingly

--- Simscape_Matlab.m
+++ Simscape_Matlab.m
@@ -13,13 +13,14 @@ for S = 1:4
 model = 'MassSpringDamper_simulink';
 open_system(model);
 Smodel = sim(model);
-    
+
+% Assigns Data based on road profile simulation to "Data" struct 
 names = {'Smooth Speed Bump','Rough Terrain','Smooth Median Strip','Rough Terrain + Pothole'};
 Data(S).name   = names{S};
 Data(S).t      = Smodel.tout;
+Data(S).Zt     = Smodel.RoadProfile.Data;
 Data(S).S_pos  = Smodel.SprungMassPos.Data;
 Data(S).US_pos = Smodel.UnSprungMassPos.Data;
-Data(S).Zt     = Smodel.RoadProfile.Data;
 Data(S).S_vel  = Smodel.SprungMassVel.Data;
 Data(S).US_vel = Smodel.UnSprungMassVel.Data;
 Data(S).S_Acc  = Smodel.SprungMassAccel.Data;
@@ -30,8 +31,16 @@ Data(S).comfort    = rms(Data(S).S_Acc);
 Data(S).maxTravel  = max(Data(S).S_pos);
 Data(S).deflection = Data(S).US_pos - Data(S).Zt;
 
+figure(1)
+set(gcf, 'Name', 'Car position')
 subplot(2,2,S)
 plot(Data(S).t, Data(S).S_pos)
-title(Data(S).name)
+title(Data(S).name) 
+
+figure(2)
+set(gcf, 'Name', 'Tire position')
+subplot(2,2,S)
+plot(Data(S).t, Data(S).US_pos)
+title(Data(S).name, 'Tire position')
 
 end
